124fb9e05a71de43a5629e86c317f77d007060c0
ali purnama alam <alipurnama69@gmail.com>
2025-03-11 14:10:52 +0700

    fix(dock) fix err code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,22 +21,22 @@ export default function Home() {
     }, []);
 
     return (
-        <main className="flex">
+        <main className="flex flex-col ">
             <div className="flex p-10 max-md:flex-col w-full justify-center items-center h-screen">
-                <div className="flex flex-col gap-10 w-3/3">
-                    <TypingAnimation className="text-7xl dark:text-milk-white " startOnView={true} >ALI PURNAMA ALAM</TypingAnimation>
-                    <div className="flex flex-col" >
-                        <h1 className="text-xl font-bold text-black dark:text-white" >Crafting digital experiences</h1>
+                <div className="flex max-md:absolute top-50 flex-col max-md: max-md:gap-3 max-md:w-full max-md:justify-start max-md:items-center gap-10 w-3/3">
+                    <TypingAnimation className="text-7xl max-md:text-xl  dark:text-white" >ALI PURNAMA ALAM</TypingAnimation>
+                    <div className="flex gap-0 max-md:justify-center max-md:items-center flex-col" >
+                        <h1 className="text-xl max-md:text-sm font-bold text-black dark:text-white" >Crafting digital experiences</h1>
                         <div className="flex items-center gap-2 " >
-                            <h1 className="text-xl font-bold text-black dark:text-white" >with passion and precision as a</h1>
+                            <h1 className="text-xl max-md:text-sm font-bold text-black dark:text-white" >with passion and precision as a</h1>
                             <WordRotate
-                                className="text-xl font-bold text-black dark:text-white"
-                                words={["Web Developer", "FrontEnd Developer", "React JS Developer", "ICT Enthusiast"]}
+                                className="text-xl max-md:text-sm font-bold text-black dark:text-white"
+                                words={["Web Dev", "FrontEnd Dev", "React JS Dev", "ICT Enthusiast"]}
                             />
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-center items-center w-full h-full">
+                <div className="flex justify-center items-center w-full max-md:absolute top-30 ">
                     {isMobile ? (
                         <Spline  scene={"/models/macbook-mobile.spline"} />
                     ) : (
